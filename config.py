d1dd0ffd5722819ca906c1622d7d3a77d1e63372
Bruno Mileto <brunomileto@outlook.com>
2020-11-23 06:59:04 -0300

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from datetime import datetime
 import logging
 import time
 
-API_KEY_TEST = 'VdBnQ9rM60MqELvvII4m7Pi6SBk06Urt6AAG2ixMTdX5maagTWrjZlr3MIaHsW9c'
-SECRET_KEY_TEST = 'n1owlwcLarcLQNkJgwG8fdrjKgjuuE8aWiCVJcIJygLTPkHGBwPJMVO34yAJMaqK'
-API_KEY = "T8OAWzBGVHVJokT6nHxP6Eqo0HNGkl9xgK79TmUXtbQhCI1M2rXwzA4E9C7rSTyI"
-SECRET_KEY = "x6BUKF8D9tCEOT0ZyeME0Nchglngudw7HcVD7qzB1uy7BN0c7tUZJGbJDXFrJKD2"
+API_KEY_TEST = ''
+SECRET_KEY_TEST = ''
+API_KEY = ""
+SECRET_KEY = ""
 API_TEST_URL = 'https://testnet.binance.vision/api'
 TODAY = datetime.now().date().strftime("%d_%m_%y")
 
